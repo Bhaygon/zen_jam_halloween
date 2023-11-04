55c415a29b3889024b3e79c9dc06faf8d1f09809
Eduardo Morato <eduardohenrique110199@gmail.com>
2023-11-04 08:54:38 -0300

    Tela de créditos

--- bhaygon_workplace/hud/hud.gd
+++ bhaygon_workplace/hud/hud.gd
@@ -8,16 +8,19 @@ func new_game_screen(message: String):
     show_message(message)
     $StartContainer/VBoxContainer/Button.set_deferred("visible", true)
     $StartContainer.set_deferred("visible", true)
+    $CreditsButton.set_deferred("visible", true)
 
 func hide_message():
     $StartContainer/VBoxContainer/Button.set_deferred("visible", false)
     $StartContainer.set_deferred("visible", false)
+    
 
 func show_message(message: String):
     $StartContainer/VBoxContainer/Title.text = message
 
 func hide_play():
     $StartContainer/VBoxContainer/Button.set_deferred("visible", false)
+    $CreditsButton.set_deferred("visible", false)
 
 func update_score(value):
     $Display/HBoxContainer/ScoreText.text = str(value)
@@ -28,3 +31,10 @@ func update_jumps(value):
 
 func _on_button_pressed():
     start.emit()
+
+
+func _on_credits_button_pressed():
+    $CreditsScreen.set_deferred("visible", true)
+
+func _on_back_credits_button_pressed():
+    $CreditsScreen.set_deferred("visible", false)
